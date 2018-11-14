538ef1a337684fccbc3080fab4cd7f0056cfb5f5
Test458000 <ya.sergey@ukr.net>
2018-11-14 19:14:56 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+window.onload function ped_go () {
+	let a = document.body.innerHTML;
+	document.body.innerHTML = a.replace(/Россия/g, 'Педерация');
+	a = document.body.innerHTML;
+	document.body.innerHTML = a.replace(/России/g, 'Педерации');
+	a = document.body.innerHTML;
+	document.body.innerHTML = a.replace(/Россией/g, 'Педерацией');
+	a = document.body.innerHTML;
+	document.body.innerHTML = a.replace(/Росія/g, 'Педерація');
+	a = document.body.innerHTML;
+	document.body.innerHTML = a.replace(/Росії/g, 'Педерації');
+	a = document.body.innerHTML;
+	document.body.innerHTML = a.replace(/Росією/g, 'Педерацією');
+	let b = document.title;
+	document.title = b.replace(/Россия/g, 'Педерация');
+	b = document.title;
+	document.title = b.replace(/России/g, 'Педерации');
+	b = document.title;
+	document.title = b.replace(/Россией/g, 'Педерацией');
+	b = document.title;
+	document.title = b.replace(/Росія/g, 'Педерація');
+	b = document.title;
+	document.title = b.replace(/Росії/g, 'Педерації');
+	b = document.title;
+	document.title = b.replace(/Росією/g, 'Педерацією');
+}
+
+ped_go ();
