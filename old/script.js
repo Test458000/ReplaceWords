d45e84be0fb8e61b6bb95089b9f1631790fca1a3
Test458000 <ya.sergey@ukr.net>
2018-11-16 13:01:27 +0200

    Optimization #1

---
+++ old/script.js
@@ -0,0 +1,37 @@
+function ped_go () {
+	// body change
+	let a = document.body.innerHTML;
+	document.body.innerHTML = a.replace(/Россия/g, 'Педерация');
+	a = document.body.innerHTML;
+	document.body.innerHTML = a.replace(/России/g, 'Педерации');
+	a = document.body.innerHTML;
+	document.body.innerHTML = a.replace(/Россией/g, 'Педерацией');
+	a = document.body.innerHTML;
+	document.body.innerHTML = a.replace(/Росія/g, 'Педерація');
+	a = document.body.innerHTML;
+	document.body.innerHTML = a.replace(/Росії/g, 'Педерації');
+	a = document.body.innerHTML;
+	document.body.innerHTML = a.replace(/Росією/g, 'Педерацією');
+	a = document.body.innerHTML;
+	document.body.innerHTML = a.replace(/Росси́я/g, 'Педерация');
+	a = document.body.innerHTML;
+	document.body.innerHTML = a.replace(/Россию/g, 'Педерацию');
+	a = document.body.innerHTML;
+	document.body.innerHTML = a.replace(/Росію/g, 'Педерацію');
+	
+	// Title change
+	let b = document.title;
+	document.title = b.replace(/Россия/g, 'Педерация');
+	b = document.title;
+	document.title = b.replace(/России/g, 'Педерации');
+	b = document.title;
+	document.title = b.replace(/Россией/g, 'Педерацией');
+	b = document.title;
+	document.title = b.replace(/Росія/g, 'Педерація');
+	b = document.title;
+	document.title = b.replace(/Росії/g, 'Педерації');
+	b = document.title;
+	document.title = b.replace(/Росією/g, 'Педерацією');
+}
+
+ped_go ();
--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 function ped_go () {
-	// body change
-	let a = document.body.innerHTML;
-	document.body.innerHTML = a.replace(/Россия/g, 'Педерация');
-	a = document.body.innerHTML;
-	document.body.innerHTML = a.replace(/России/g, 'Педерации');
-	a = document.body.innerHTML;
-	document.body.innerHTML = a.replace(/Россией/g, 'Педерацией');
-	a = document.body.innerHTML;
-	document.body.innerHTML = a.replace(/Росія/g, 'Педерація');
-	a = document.body.innerHTML;
-	document.body.innerHTML = a.replace(/Росії/g, 'Педерації');
-	a = document.body.innerHTML;
-	document.body.innerHTML = a.replace(/Росією/g, 'Педерацією');
-	a = document.body.innerHTML;
-	document.body.innerHTML = a.replace(/Росси́я/g, 'Педерация');
-	a = document.body.innerHTML;
-	document.body.innerHTML = a.replace(/Россию/g, 'Педерацию');
-	a = document.body.innerHTML;
-	document.body.innerHTML = a.replace(/Росію/g, 'Педерацію');
+	
+	const dictRu = {
+		//Rus
+		'Россия' : 'Педерация',
+		'России' : 'Педерации',
+		'Россией' : 'Педерацией',
+		'Росси́я' : 'Педерация',
+		'Россию' : 'Педерацию',
+	};
+	const dictUa = {
+		//Ukrainian
+		'Росія' : 'Педерація',
+		'Росії' : 'Педерації',
+		'Росією' : 'Педерацією',
+		'Росію' : 'Педерацію',
+		'Росі́я' : 'Педерація',
+	};
+	
+	let lng = document.getElementsByTagName("html")[0].getAttribute("lang");;
+
+	if (lng === "ru"){
+		dict = dictRu;
+	}
+	else if (lng === "uk"){
+		dict = dictUa;
+	}
+	else return;
+	
+	// body change	
+	let b = document.body.innerHTML;	
+	for (let i in dict) {
+		b = b.replace(new RegExp(i,"g"), dict[i]);
+	}
+	document.body.innerHTML = b;
 	
 	// Title change
-	let b = document.title;
-	document.title = b.replace(/Россия/g, 'Педерация');
-	b = document.title;
-	document.title = b.replace(/России/g, 'Педерации');
-	b = document.title;
-	document.title = b.replace(/Россией/g, 'Педерацией');
-	b = document.title;
-	document.title = b.replace(/Росія/g, 'Педерація');
-	b = document.title;
-	document.title = b.replace(/Росії/g, 'Педерації');
-	b = document.title;
-	document.title = b.replace(/Росією/g, 'Педерацією');
+	let t = document.title;
+	for (let i in dict) {
+		t = t.replace(new RegExp(i,"g"), dict[i]);
+	}
+	document.title = t;
 }
 
 ped_go ();
