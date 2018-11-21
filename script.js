2eca94fd6b4169279b1b2a7b90ca3797f2026647
Test458000 <ya.sergey@ukr.net>
2018-11-21 20:37:41 +0200

    Use Dictionary from the server #2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function ped_go () {
 	
-	const dictRu = {
+/*	const dictRu = {
 		//Rus
 		'Россия' : 'Педерация',
 		'России' : 'Педерации',
@@ -16,9 +16,40 @@ function ped_go () {
 		'Росію' : 'Педерацію',
 		'Росі́я' : 'Педерація',
 	};
+*/
 	
-	let lng = document.getElementsByTagName("html")[0].getAttribute("lang");;
+	// Загрузка и обработка словарей по ссылке 
+	
+	function get_dict(lng_res) {
+		let temp;
+		fetch ("http://sbox.pp.ua/ext/dict.json", {method: 'GET'}) // Запрос на сервер
+			.then (response => {
+				let dict_res = response.json();
+				return  dict_res;
+			})
+			.then ( function (dict_res) {  // Создание нового массива из объекта Request 
+				let lng_res = {};	
+				for (let i in dict_res) {
+					let n = 0;
+					lng_res.n = i;
+					temp = dict_res[i];			
+				};	
+				alert (temp);
+				return temp;
+			});
+		alert (temp);
+		return  lng_res;
+	};
+	
+	const dict_all = get_dict();
+	alert (dict_all);
+	
+	let lng = document.getElementsByTagName("html")[0].getAttribute("lang");; // Получение информации о текущем языке страницы
 
+	// Замена слов на странице
+	
+//	let lang_i = '\"'+i+'\"';
+	
 	if (lng === "ru"){
 		dict = dictRu;
 	}
@@ -27,18 +58,13 @@ function ped_go () {
 	}
 	else return;
 	
-	// body change	
-	let b = document.body.innerHTML;	
+	let b = document.body.innerHTML; //body change
+	let t = document.title;	 //title change
 	for (let i in dict) {
 		b = b.replace(new RegExp(i,"g"), dict[i]);
-	}
-	document.body.innerHTML = b;
-	
-	// Title change
-	let t = document.title;
-	for (let i in dict) {
 		t = t.replace(new RegExp(i,"g"), dict[i]);
 	}
+	document.body.innerHTML = b;
 	document.title = t;
 }
 
