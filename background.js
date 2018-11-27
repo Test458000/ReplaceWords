76facbec0e099645257fc93ccb6d996a3ef538b1
Test458000 <ya.sergey@ukr.net>
2018-11-27 21:02:23 +0200

    Use Dictionary from the server #2: ver 1.55

--- background.js
+++ background.js
@@ -2,9 +2,17 @@
 	
 const config = {
 	dictUrl : "http://sbox.pp.ua/ext/dict.json",
+	tagList : ["a", "abbr", "address", "area", "article", "aside", "audio", "b", "base", "bdi", "bdo", "blockquote", "body", "br", "button", "canvas", "caption", "cite", "code", "col", "colgroup", 
+		"datalist", "dd", "del", "details", "dfn", "dialog", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "footer", "form", 
+		"h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "label", "legend", "fieldset", "li", "link", 
+		"main", "map", "mark", "menu", "menuitem", "meta", "meter", "nav", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", 
+		"q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strong", "style", "sub", "summary", "sup", 
+		"table", "tbody", "td", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "ul", "var", "video", "wbr"
+	],
 };
 	
-let dictRes;
+let dictRes, tagN, res, dictLast;
+let s = false;
 
 chrome.extension.onMessage.addListener (function (req) {   // Get a request for run this script
    if (req == 'get me a dictionary') {
@@ -18,27 +26,94 @@ function setDict () {                // Load and write a dictionary
 			let resultResp = response.json();
 			return resultResp;
 		})
-		.then ( dictRes => {           // Create a new object from Request and write it to Chrome
-			chrome.storage.local.set(dictRes, function () {
-				getDict ();
-				// alert(dictRes);
+		.then ( dictRes => {           // Create a new object from Request
+			dictLast = checkDictWords (dictRes);
+			return dictLast;
+		})
+		.then ( dictLast => {           // Write a dictionary to Chrome
+			chrome.storage.local.set(dictLast, function () {
+			//dictRead(dictRes);
+			//alert (dictLast);
+			//dictRead(dictLast);
 			});	
-			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {  // Return a dictionary to the main script
-				chrome.tabs.sendMessage(tabs[0].id, {dictRes});				
+		return dictLast;
+		})
+		.then ( dictLast => {  			// Return a dictionary to the main script
+			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+				chrome.tabs.sendMessage(tabs[0].id, {dictLast});				
 			});	
 		});
 };
 
+function dictRead(dictRes) {
+	for (let i in dictRes) {
+		let dictResI = dictRes[i];
+		//alert(i);
+		//alert(dictRes[i]);
+		for (let j in dictResI) {
+			alert (j);
+			alert (dictResI[j]);
+		}
+	}		
+};
+
+function checkDictWords(dictRes){
+	getDict ();
+	for (let i in dictRes) {
+		let dictResI = dictRes[i];
+		for (let j in dictResI) {
+			for (n in config.tagList) {
+				if (j == config.tagList[n] || dictResI[j] == config.tagList[n]) {
+					s = true;
+					//alert (j);
+				};
+			};
+			if (s == true) {
+				delete dictResI[j];
+				j = undefined;
+				s = false;
+			};	
+		};
+		dictRes[i] = dictResI;
+	}
+	//dictRead(dictRes);
+	return dictRes;	
+};
+
 function getDict () {      // Get a dictionary and transfer into a script.js
 	chrome.storage.local.get(dictRes, function (res) {
-		dictRes = res;
-	});			
+		dictRes = res;			
+	});
+	return dictRes;	
 };
 	
 // Start this script
 //setDict ();
 
 // Temporary code
+
+
+/*function checkDictWords(dictRes) { 		// Check a dictionary for bad words
+	let dictResI, nJ, dictTemp, dictTempJ;
+	for (let i in dictRes) {
+		dictResI = dictRes[i];
+		for (let j in dictResI) {
+			for (n in config.tagList) {
+				if (j == config.tagList[n] || dictResI[j] == config.tagList[n]) {
+					s = true;
+					alert (j);
+				};
+			};
+			if (s == true) {
+				delete dictResI['\"' + j +'\"'];
+				j = undefined;
+				s = false;
+			};	
+		};
+		dictRes[i] = dictResI;
+	};
+	return dictRes;
+}*/
 
 //     	let lng = document.getElementsByTagName("html")[0].getAttribute("lang");; // Получение информации о текущем языке страницы	
 //		if (langi == lng || langiTwice == lng) {	Don't work in many pages
--- script.js
+++ script.js
@@ -5,33 +5,99 @@ function checkDict () {             // Check is a dicionary exist
 	if (!dict) {
 		chrome.extension.sendMessage('get me a dictionary');
 	} 
-	else replaceWords();
+	else hrome.storage.local.get(dictLast, function (res) {
+		dict = dictLast;
+		replaceWords(dict);
+	});	
 };
 
 chrome.runtime.onMessage.addListener( 			 //Get a dictionary from a background page
   function(request, sender, sendResponse) {
-    if (request.dictRes){
-		dict = request.dictRes;
-		replaceWords ();
+    if (request.dictLast){
+		dict = request.dictLast;
+		//alert (dict);
+		replaceWords (dict);
 	};
   });	
 
-function replaceWords () {     	 // Replace words on a page from dictionary
-		let bodyPage = document.body.innerHTML; 		//body change
-		let titlePage = document.title;			 //title change
+function replaceWords (dict) {      // Replace words on a page from dictionary
+		//alert (dict);
+		let bodyPage = document.body.innerHTML; //body change
+		let titlePage = document.title;	 //title change
 		for (let i in dict) {
-			let dicti = dict[i];
-//			alert (i);
-			for (let j in dicti) {
-				bodyPage = bodyPage.replace(new RegExp(j,"g"), dicti[j]);
-				titlePage = titlePage.replace(new RegExp(j,"g"), dicti[j]);
+			let dictI = dict[i];
+			//alert (i);
+			for (let j in dictI) {
+				bodyPage = bodyPage.replace(new RegExp(j,"g"), dictI[j]);
+				titlePage = titlePage.replace(new RegExp(j,"g"), dictI[j]);
 			}
 		}
 		document.body.innerHTML = bodyPage;
 		document.title = titlePage;		
 };
 
+function dictRead(dictRes) {
+	for (let i in dictRes) {
+		let dictResI = dictRes[i];
+		//alert(i);
+		//alert(dictRes[i]);
+		for (let j in dictResI) {
+			alert (j);
+			alert (dictResI[j]);
+		}
+	}		
+};
+
 // Temporary code
+
+/*  
+
+	let bodyPage = document.body.getElementsByTagName('*'); 		//body change
+	//alert(bodyPage);
+	let titlePage = document.title;			 //title change
+		
+	for (let n = 0; n < bodyPage.length; n++) {    // Look over HTML-collection
+		//let bodyPageN = bodyPage[n].innerText;
+		//alert(bodyPageN);
+		//alert(bodyPage[n].tagName);
+		//let index = items.indexOf(bodyPage[n]);
+		//alert(index);
+		for (let i in dict) {
+			let dictI = dict[i];
+			//alert (i);
+			for (let j in dictI) {
+				//alert(dictI[j]);
+				//alert (j);					
+				bodyPage[n].textContent = bodyPage[n].textContent.replace(new RegExp(j,"g"),dictI[j]);
+				//bodyPage.item(n) = bodyPageN;
+				//alert(bodyPageN);
+				titlePage = titlePage.replace(new RegExp(j,"g"), dictI[j]);
+			}
+		}
+	}   
+	
+	alert(bodyPage);
+	//document.body.getElementsByTagName('*') = bodyPage;
+	//document.body.innerHTML = bodyPage;
+	document.title = titlePage;		
+};
+
+
+function replaceWords () {      // Replace words on a page from dictionary
+			let bodyPage = document.body.textContent; //body change
+			let titlePage = document.title;	 //title change
+			for (let i in dict) {
+				let dicti = dict[i];
+				alert (i);
+				for (let j in dicti) {
+					bodyPage = bodyPage.replace(new RegExp(j,"g"), dicti[j]);
+					titlePage = titlePage.replace(new RegExp(j,"g"), dicti[j]);
+				}
+			}
+			document.body.textContent = bodyPage;
+			document.title = titlePage;				
+};
+*/
 
 //     	let lng = document.getElementsByTagName("html")[0].getAttribute("lang");; // Получение информации о текущем языке страницы	
 //		if (langi == lng || langiTwice == lng) {	Don't work in many pages
