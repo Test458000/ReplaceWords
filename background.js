511600e39ec51b670698fb5be8c50bf83fdada19
Mykhailo Onikiienko <michael.onikienko@gmail.com>
2018-12-08 16:51:17 +0200

    Format a coded

--- background.js
+++ background.js
@@ -1,27 +1,29 @@
 // Load a dictionary by url
-	
+
 const config = {
-	dictUrl : "http://sbox.pp.ua/ext/dict.json",
-	};
+    dictUrl: "http://sbox.pp.ua/ext/dict.json",
+};
 
 // Load and write a dictionary
-function setDict () {
-	fetch (config.dictUrl)          
-		.then (response => {
-			let dictRes = response.json();
-			return dictRes;
-		}).then ( dictRes => {
-			// Write a dictionary to chrome.storage
-			let dict = dictRes;
-			chrome.storage.local.set({dictionary: dict});
-		});
-}
-
-function checkDict () {
-	// Check is a dictionary exist
-    chrome.storage.local.get (['dictionary'], function (res) {
-        if (!res.dictionary) {setDict ()}
+function setDict() {
+    fetch(config.dictUrl)
+        .then(response => {
+            let dictRes = response.json();
+            return dictRes;
+        }).then(dictRes => {
+        // Write a dictionary to chrome.storage
+        let dict = dictRes;
+        chrome.storage.local.set({dictionary: dict});
     });
 }
 
-checkDict ();
+function checkDict() {
+    // Check is a dictionary exist
+    chrome.storage.local.get(['dictionary'], function (res) {
+        if (!res.dictionary) {
+            setDict()
+        }
+    });
+}
+
+checkDict();
--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-chrome.storage.local.get (['dictionary'], function (res) {
+chrome.storage.local.get(['dictionary'], function (res) {
     if (res.dictionary) {
         let node = document.body;
         let dict = res.dictionary;
         //delete dict.en.div;
-        walkDom (node, dict, replaceWords);
+        walkDom(node, dict, replaceWords);
     }
 });
 
 //Walk on each element into DOM
-function walkDom (elem, dict, replaceWords) {
+function walkDom(elem, dict, replaceWords) {
     for (let n = 0; n < elem.childNodes.length; n++) {
         let node = elem.childNodes[n];
         if (node.nodeType === 3) {
-            replaceWords (node, dict);
+            replaceWords(node, dict);
         } else if (node.nodeType === 1 && node.nodeName !== "SCRIPT" && node.tagName !== "STYLE") {
             walkDom(node, dict, replaceWords);
         }
     }
 }
 
-function replaceWords (node, dict) {
+function replaceWords(node, dict) {
     let titlePage = document.title;
     for (let i in dict) {
         let dictI = dict[i];
         // Loops for replace words for this language
         for (let j in dictI) {
             // Loops each text for replace words and replace text
-            if (node.data.indexOf(j)!== -1) {
+            if (node.data.indexOf(j) !== -1) {
                 let textInNode = node.data;
                 node.data = textInNode.replace(new RegExp(j, "g"), dictI[j]);
                 titlePage = titlePage.replace(new RegExp(j, "g"), dictI[j]);
